9b6832eecc03254a37b0cac228a11c83defb5ab2
Sara García <up202306877@up.pt>
2024-12-14 23:52:12 +0000

    Board, BoardBuilder alterações

--- src/main/java/com/towerdefense/model/game/board/BoardBuilder.java
+++ src/main/java/com/towerdefense/model/game/board/BoardBuilder.java
@@ -1,4 +1,19 @@
 package com.towerdefense.model.game.board;
 
-public class BoardBuilder {
+import com.towerdefense.model.game.elements.enemies.Enemy;
+import com.towerdefense.model.game.elements.towers.Tower;
+
+import java.util.List;
+
+public abstract class BoardBuilder {
+    public Board createArena() {
+        Board board = new Board(getWidth(), getHeight());
+
+        return board;
+    }
+
+    protected abstract int getWidth();
+
+    protected abstract int getHeight();
+
 }
