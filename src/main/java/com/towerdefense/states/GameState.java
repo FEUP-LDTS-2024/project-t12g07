d8cb2750601eaa8f3d7cc8a23bcbaee666a6704d
Sara García <up202306877@up.pt>
2024-12-14 17:05:56 +0000

    ALTERACOES

--- src/main/java/com/towerdefense/states/GameState.java
+++ src/main/java/com/towerdefense/states/GameState.java
@@ -1,4 +1,23 @@
 package com.towerdefense.states;
 
-public class GameState {
+import com.towerdefense.controllers.Controller;
+import com.towerdefense.controllers.game.BoardController;
+import com.towerdefense.model.game.board.Board;
+import com.towerdefense.viewer.Viewer;
+import com.towerdefense.viewer.game.GameViewer;
+
+public class GameState extends State<Board> {
+    public GameState(Board board) {
+        super(board);
+    }
+
+    @Override
+    protected Viewer<Board> getViewer() {
+        return new GameViewer(getModel());
+    }
+
+    @Override
+    protected Controller<Board> getController() {
+        return new BoardController(getModel());
+    }
 }
--- src/main/java/com/towerdefense/states/MenuState.java
+++ src/main/java/com/towerdefense/states/MenuState.java
@@ -1,4 +1,23 @@
 package com.towerdefense.states;
 
-public class MenuState {
+import com.towerdefense.controllers.Controller;
+import com.towerdefense.controllers.menu.MenuController;
+import com.towerdefense.model.menu.Menu;
+import com.towerdefense.viewer.Viewer;
+import com.towerdefense.viewer.menu.MenuViewer;
+
+public class MenuState extends State<Menu> {
+    public MenuState(Menu model) {
+        super(model);
+    }
+
+    @Override
+    protected Viewer<Menu> getViewer() {
+        return new MenuViewer(getModel());
+    }
+
+    @Override
+    protected Controller<Menu> getController() {
+        return new MenuController(getModel());
+    }
 }
